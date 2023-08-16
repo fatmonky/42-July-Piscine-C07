e2f281de89e3361ab0aa04ba38b7f5f3cfd4752a
Peijing Teh <pteh@c1r1s2.42singapore.sg>
2023-08-16 11:52:21 +0800

    ex03 WIP: figuring out concat logic for 2D char array.

--- ex02/ft_ultimate_range.c
+++ ex02/ft_ultimate_range.c
@@ -6,7 +6,7 @@
 /*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 17:34:01 by pteh              #+#    #+#             */
-/*   Updated: 2023/08/16 11:05:40 by pteh             ###   ########.fr       */
+/*   Updated: 2023/08/16 11:09:25 by pteh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,15 @@ int	ft_ultimate_range(int **range, int min, int max)
 	int	arrsize;
 	int	*array;
 	int	index;
-	//function should allocate and assign all values between min (inclusive) and max (excluded)
-	//into an array of ints.
 
 	arrsize = max - min;
 	array = (int *)malloc(arrsize * sizeof(int));
 	index = 0;
-	//if min >= max, range points on NULL and return 0.
 	if (min >= max)
 	{
 		*range = NULL;
 		return (0);
 	}
-	//function should return range (or -1 on error)
 	while (index < arrsize)
 	{
 		if (array == NULL)
@@ -39,13 +35,10 @@ int	ft_ultimate_range(int **range, int min, int max)
 		index++;
 	}
 	*range = array;
-	return(arrsize);
+	return (arrsize);
 }
-//15 Aug still: trying to figure out how to test this, before I proceed with this code...
-// 16 Aug: testing this out using Jin Liang's test function.
 
-//to comment out before submission, with main.
-
+/*
 int	main(void)
 {
 	int	min = -22;
@@ -57,4 +50,4 @@ int	main(void)
 	for (int i = 0; i < size; i++)
 		printf("array1[%d] for -22, 120 : [%d]\n", i, range[i]);
 	free(range);
-}
+}*/
